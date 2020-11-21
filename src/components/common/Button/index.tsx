3b676c3c919aaef709f54d99660bb6dffc2f2ca6
NagaiKoki <kokinagai10+1@gmail.com>
2020-11-21 17:18:37 +0900

    fix styles

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 // import styles
-import { COLOR } from '../../../../styles'
+import { COLOR, FONT_SIZE } from '../../../../styles'
 
 type Props = {
   text: string
@@ -12,7 +12,7 @@ const Button: React.FC<Props> = props => {
   const { text, onClick } = props
 
   return (
-    <Wrapper>
+    <Wrapper onClick={onClick}>
       {text}
     </Wrapper>
   )
@@ -24,9 +24,10 @@ const Wrapper = styled.button`
   border-radius: 4px;
   color: ${COLOR.WHITE_COLOR};
   background: ${COLOR.MAIN_COLOR};
-  padding: 5px 13px;
+  font-size: ${FONT_SIZE.BASE_TEXT_SIZE};
   font-weight: bold;
   text-align: center;
+  padding: 7px 15px;
   &:hover {
     opacity: 0.5;
   }
--- styles/index.ts
+++ styles/index.ts
@@ -3,3 +3,20 @@ export const COLOR = {
   WHITE_COLOR: '#fff',
   BOX_SHADOW_UNDER: 'rgba(0, 0, 0, 0.03) 0px 5px 5px;'
 }
+
+export const FONT_SIZE = {
+  BASE_TEXT_SIZE: '14px',
+  FS_10: '10px',
+  FS_11: '11px',
+  FS_12: '12px',
+  FS_13: '13px',
+  FS_15: '15px',
+  FS_16: '16px',
+  FS_17: '17px',
+  FS_18: '18px',
+  FS_19: '19px',
+  FS_20: '20px',
+  FS_21: '21px',
+  FS_22: '22px',
+  FS_23: '23px',
+}

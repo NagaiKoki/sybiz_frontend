c2e0b3c618639bd0fb60e4dd13281b1e8eb5c5d5
NagaiKoki <kokinagai10+1@gmail.com>
2020-11-22 10:12:56 +0900

    creater env file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,27 +13,8 @@ export default class AppDocument extends Document {
           <meta name="viewport" content="width=device-width,initial-scale=1" />
         </Head>
         <body>
-        <script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js"></script>
-        <script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-analytics.js"></script>
-        <script 
-          dangerouslySetInnerHTML={{
-            __html:  `
-              var firebaseConfig = {
-                apiKey: "AIzaSyB2wVoccdRo-69iU7KHJnpQIEKZC98DpTI",
-                authDomain: "hukugyo-ddc0c.firebaseapp.com",
-                databaseURL: "https://hukugyo-ddc0c.firebaseio.com",
-                projectId: "hukugyo-ddc0c",
-                storageBucket: "hukugyo-ddc0c.appspot.com",
-                messagingSenderId: "851741893356",
-                appId: "1:851741893356:web:f4399bcbb64ee89bda3bc9",
-                measurementId: "G-K1EZC379HX"
-              };
-              // Initialize Firebase
-              firebase.initializeApp(firebaseConfig);
-              firebase.analytics();
-            `
-          }}
-        />
+          <script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js"></script>
+          <script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-analytics.js"></script>
           <Main />
           <NextScript />
         </body>

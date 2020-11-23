a646ac3c8ec25c85f764049003d6ca4a082e1e75
NagaiKoki <kokinagai10+1@gmail.com>
2020-11-23 13:44:43 +0900

    setUser

--- src/sagas/auth.ts
+++ src/sagas/auth.ts
@@ -8,18 +8,21 @@ import {
   successSignIn,
   failureSignIn
 } from '../slices/auth'
+import { setLoginUser } from '../slices/public/user'
 // import types
 import { RequestSignInType } from '../types/auth'
 import { ResponseType } from '../types'
+import { PublicUserType } from '../types/public/user'
 
 function* runRequestSignIn(action: PayloadAction<RequestSignInType>) {
-  const { payload, error }: ResponseType<any> = yield call(
+  const { payload, error }: ResponseType<PublicUserType> = yield call(
     requestFetchSignIn,
     action.payload.type
   )
 
   if (payload && !error) {
     yield put(successSignIn())
+    yield put(setLoginUser(payload))
   } else {
     yield put(failureSignIn(error))
   }

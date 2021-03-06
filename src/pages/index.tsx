e33e67c8dea74fff5041c929bca62cf18f1bce4c
NagaiKoki <kokinagai10+1@gmail.com>
2021-03-06 17:43:40 +0900

    make ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,98 +1,89 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import styled from 'styled-components'
-import Image from 'next/image'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+// import Image from "next/image";
 // import styles
-import { COLOR, FONT_SIZE } from '../../styles'
+import { COLOR, FONT_SIZE } from "../../styles";
 // import components
-import Button from '../components/common/Button'
+import Button from "../components/common/Button";
 // import dispatchers
-import { useUiDispatchers } from '../dispatchers/ui'
+import { useUiDispatchers } from "../dispatchers/ui";
 
-const Index: React.FC = props => { 
-  const { toggleAuthModalOpen } = useUiDispatchers()
-  
+const Index: React.FC = (props) => {
+  const { toggleAuthModalOpen } = useUiDispatchers();
+
   return (
     <Wrapper>
       <MainWrapper>
-        <Title>
-          副業を募集しよう
-        </Title>
+        <Title>副業を募集しよう</Title>
         <ImageWrapper>
-          <Image src={'/top_image.png'} width={250} height={200} />
+          {/* <Image src={"/top_image.png"} width={250} height={200} /> */}
         </ImageWrapper>
         <ButtonWrapper>
           <Button text="募集する" size="medium" onClick={toggleAuthModalOpen} />
         </ButtonWrapper>
       </MainWrapper>
       <DescriptionWrapper>
-        <SubTitle>
-          説明説明説明
-        </SubTitle>
+        <SubTitle>説明説明説明</SubTitle>
         <Paragraph>
           ほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげ
         </Paragraph>
       </DescriptionWrapper>
       <DescriptionWrapper>
-        <SubTitle>
-          説明説明説明
-        </SubTitle>
+        <SubTitle>説明説明説明</SubTitle>
         <Paragraph>
           ほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげ
         </Paragraph>
       </DescriptionWrapper>
       <DescriptionWrapper>
-        <SubTitle>
-          説明説明説明
-        </SubTitle>
+        <SubTitle>説明説明説明</SubTitle>
         <Paragraph>
           ほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげ
         </Paragraph>
       </DescriptionWrapper>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
 
-const Wrapper = styled.div`
-`
+const Wrapper = styled.div``;
 
 const MainWrapper = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-`
+`;
 
 const Title = styled.h2`
   padding-bottom: 20px;
   color: ${COLOR.BASE_TEXT};
   font-size: ${FONT_SIZE.FS_30};
   text-align: center;
-`
+`;
 
 const ImageWrapper = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 
 const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 30px;
-`
+`;
 
 const DescriptionWrapper = styled.div`
   margin: 30px 0;
   padding: 30px;
-`
+`;
 
 const SubTitle = styled.h3`
   color: ${COLOR.BASE_TEXT};
   font-size: ${FONT_SIZE.FS_20};
-`
+`;
 
 const Paragraph = styled.p`
   color: ${COLOR.BASE_TEXT};
   font-size: ${FONT_SIZE.BASE_TEXT_SIZE};
-`
+`;
--- src/pages/onBoarding.tsx
+++ src/pages/onBoarding.tsx
@@ -1,80 +1,93 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import Image from 'next/image'
+import React, { useState } from "react";
+import styled from "styled-components";
+// import Image from "next/image";
 // import apis
-import { requestFetchGetUser } from '../apis/public/user'
+import { requestFetchGetUser } from "../apis/public/user";
 // import styles
-import { COLOR, FONT_SIZE } from '../../styles'
+import { COLOR, FONT_SIZE } from "../../styles";
 // import components
-import Label from '../components/common/Label/InputLabel'
-import TextInput from '../components/common/Input/TextInput'
+import Label from "../components/common/Label/InputLabel";
+import TextInput from "../components/common/Input/TextInput";
 // import selectors
-import { useSelectLoginUser } from '../selectors/public/user'
+import { useSelectLoginUser } from "../selectors/public/user";
 // import config
-import firebase from '../config/firebase'
+import firebase from "../config/firebase";
 
-export async function getServerSideProps() {
-  const uid = firebase.auth().currentUser.uid
-  const result = await requestFetchGetUser(uid)
-  console.log(result)
+type Props = {
+  stars: number;
+};
+
+export const getStaticProps = async (context) => {
+  const res = await fetch("https://api.github.com/repos/zeit/next.js");
+  const json = await res.json();
+  const stars = json.stargazers_count;
+  // ビルド時刻の取得
+  const build_time = new Date().toString();
   return {
-    props: {
-      user: JSON.stringify(result)
-    }
-  }
-}
+    props: { stars },
+  };
+};
 
-const OnBoardingPage: React.FC = props => {
-  console.log(firebase.auth().currentUser.uid)
-  const loginUser = useSelectLoginUser()
-  const [tempUserName, setTempUserName] = useState(loginUser?.username || '')
-  const [tempUserId, setTempUserId] = useState(loginUser?.userId || '')
+const OnBoardingPage: React.FC<Props> = (props) => {
+  console.log(props);
+  const loginUser = useSelectLoginUser();
+  const [tempUserName, setTempUserName] = useState(loginUser?.username || "");
+  const [tempUserId, setTempUserId] = useState(loginUser?.userId || "");
 
   const handleOnChangeUserName = (value: string) => {
-    setTempUserName(value)
-  }
+    setTempUserName(value);
+  };
 
   const handleOnChangeUserId = (value: string) => {
-    setTempUserId(value)
-  }
-   
+    setTempUserId(value);
+  };
+
   return (
     <Wrapper>
       <Title>フクギョウへようこそ！</Title>
+      <p>{props.stars}</p>
       <ImageWrapper>
-        <Image src="/onBoarding.png" width={200} height={180} />
+        {/* <Image src="/onBoarding.png" width={200} height={180} /> */}
       </ImageWrapper>
       <FormWrapper>
         <Label text="ユーザー名" />
         <InputWrapper>
-          <TextInput value={tempUserName} placeholder="ユーザー名" onChange={handleOnChangeUserName} />
+          <TextInput
+            value={tempUserName}
+            placeholder="ユーザー名"
+            onChange={handleOnChangeUserName}
+          />
         </InputWrapper>
       </FormWrapper>
       <FormWrapper>
         <Label text="ユーザーID" />
         <InputWrapper>
-          <TextInput value={tempUserId} placeholder="ユーザーID" onChange={handleOnChangeUserId} />
+          <TextInput
+            value={tempUserId}
+            placeholder="ユーザーID"
+            onChange={handleOnChangeUserId}
+          />
         </InputWrapper>
       </FormWrapper>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default OnBoardingPage
+export default OnBoardingPage;
 
-const Wrapper = styled.div``
+const Wrapper = styled.div``;
 
 const Title = styled.h2`
   padding-bottom: 30px;
   color: ${COLOR.BASE_TEXT};
   font-size: ${FONT_SIZE.FS_22};
   text-align: center;
-`
+`;
 
 const ImageWrapper = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 
 const FormWrapper = styled.div`
   display: flex;
@@ -82,8 +95,8 @@ const FormWrapper = styled.div`
   justify-content: center;
   width: 70%;
   margin: 20px auto;
-`
+`;
 
 const InputWrapper = styled.div`
   padding: 10px 0;
-`
+`;
